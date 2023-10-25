8a6b0ff712655939775673414569f483e91add58
alif1 <alif1@ALIF>
2023-10-25 12:53:04 +0600

    Add project files.

---
+++ FullStack API/FullStack.API/FullStack.API/Models/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Models
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions<FullStackDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
